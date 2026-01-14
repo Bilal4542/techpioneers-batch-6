e3eedda6926b615cdc5ce4d9b4857731e78d6cb0
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2026-01-14 12:31:47 +0500

    mongoose and database connected

--- 35th-class/app.js
+++ 35th-class/app.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.get('/', (req, res) =>{
     res.send('backend is running Accurately')
     })
+mongoose.connect('mongodb+srv://mbilal45422_db_user:466YhYsHLsPQUED8@cluster0.zawrjkt.mongodb.net/')
+.then(()=>{
+    console.log('Database Connected Successfully')
+}).catch((err)=>{
+    console.log('Database Connection Failed', err)
+})
+
 
 const port = 3000
 
 app.listen(port, ()=>[
     console.log('app is running on port ', port)
 ])
+
+
+// mongodb+srv://mbilal45422_db_user:466YhYsHLsPQUED8@cluster0.zawrjkt.mongodb.net/
